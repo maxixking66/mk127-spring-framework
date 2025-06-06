27425d64a83395a08fd57581fa5dde26d51453a5
maxixking66 <maxixking66@gamil.com>
2025-06-06 10:04:47 +0330

    add field injection example

--- src/main/java/ir/maktabsharif127/spring/service/FirstService.java
+++ src/main/java/ir/maktabsharif127/spring/service/FirstService.java
@@ -1,25 +1,16 @@
 package ir.maktabsharif127.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FirstService {
 
-    private String name;
-
+    @Autowired
     private SecondService secondService;
 
-    public FirstService(SecondService secondService) {
-        System.out.println("in first service constructor");
-        this.secondService = secondService;
-    }
-
     public void performLogic() {
-        System.out.println("in perform logic method in first service name is: " + name);
+        System.out.println("in perform logic method in first service");
         secondService.performLogic();
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/main/java/ir/maktabsharif127/spring/service/SecondService.java
+++ src/main/java/ir/maktabsharif127/spring/service/SecondService.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecondService {
 
-    public SecondService() {
-        System.out.println("in second service constructor");
-    }
-
     public void performLogic() {
         System.out.println("in perform logic method in second service");
     }
